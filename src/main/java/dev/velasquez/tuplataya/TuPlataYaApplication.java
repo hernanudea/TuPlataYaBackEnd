a2776502e766eb9b405cf396c51de280fc857cc7
Hernan Velasquez <hernanudea@gmail.com>
2023-11-19 16:34:50 -0500

    first commit

---
+++ src/main/java/dev/velasquez/tuplataya/TuPlataYaApplication.java
@@ -0,0 +1,14 @@
+package dev.velasquez.tuplataya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuPlataYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuPlataYaApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/dev/velasquez/tuplataya/TuPlataYaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.velasquez.tuplataya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuPlataYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
